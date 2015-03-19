397718fb5ef50865bccc660b0b71ab00e40014b7
Ahsen Masood <ahsen.m@gmail.com>
2015-03-19 00:39:15 -0400

    added ntfs config parameter

--- src/config/acl.php
+++ src/config/acl.php
@@ -1,6 +1,23 @@
 <?php
 
 return [
+
+    /**
+     * Model definitions.
+     * If you want to use your own model and extend it
+     * to package's model. You can define your model here.
+     */
+
     'role'       => 'Kodeine\Acl\Models\Eloquent\Role',
     'permission' => 'Kodeine\Acl\Models\Eloquent\Permission',
+
+    /**
+     * NTFS right, the more permissive wins
+     * If you have multiple permission aliases assigned, each alias
+     * has a common permission, view.house => false, but one alias
+     * has it set to true. If ntfs right is enabled, true value
+     * wins the race, ie the more permissive wins.
+     */
+
+    'ntfs'       => true,
 ];
