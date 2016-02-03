9ab2ca24c5f3c2c6f422aa1b959da7f2f55fedec
Benjamin Uzelac <ben@buzelac.com>
2016-02-03 11:25:53 -0600

    Fix 5.2 config for Permission too

--- src/Kodeine/Acl/Models/Eloquent/Permission.php
+++ src/Kodeine/Acl/Models/Eloquent/Permission.php
@@ -37,7 +37,7 @@ class Permission extends Model
      */
     public function users()
     {
-        return $this->belongsToMany(config('auth.model'))->withTimestamps();
+        return $this->belongsToMany(config('auth.providers.users.model', config('auth.model')))->withTimestamps();
     }
 
     /**
